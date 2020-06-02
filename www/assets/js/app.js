02792ada644fd71e0e4838d9b6bac4d821432d09
DESKTOP-Q5T7PTR\Mpho <mpho@spazapp.co.za>
2020-06-02 13:10:51 +0200

    no message

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -83,7 +83,7 @@ var DependantTypes = [
     "Spouse", "Child", "Relative"
 ];
 
-var appVersion = '2.2.8';
+var appVersion = '2.3.0';
 
 var baseUrl = "https://admin.insurapp.co.za";
 //var baseUrl = "http://demo.insurapp.co.za"; 
